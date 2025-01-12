5881630bdd0f693aeeedf4c2341f08c560a30e75
ValentinoBellini <valentinobellini.dev@gmail.com>
2025-01-12 22:42:22 +0100

    feat: bonus onf/off

--- js/script.js
+++ js/script.js
@@ -6,7 +6,17 @@ const button = document.getElementById('button');
 
 // gestiamo il cambio immagine
 button.addEventListener('click', function(){
+    const isOff = lamp.src.includes('white_lamp') && button.classList.contains('off');
     
-    lamp.src = './img/yellow_lamp.png';
-    button.className = 'on';
+    if(isOff){
+        lamp.src = './img/yellow_lamp.png';
+        button.className = 'on';
+        button.innerHTML = 'SPEGNI';
+    } else {
+        lamp.src = './img/white_lamp.png';
+        button.className = 'off';
+        button.innerHTML = 'ACCENDI';
+    }
 });
+
+
