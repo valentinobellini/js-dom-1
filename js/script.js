d6be9d808e542ba6b93cecf266c924ff0e187816
ValentinoBellini <valentinobellini.dev@gmail.com>
2025-01-12 21:18:05 +0100

    feat: html, css structure

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// selezioni elementi
+const lamp = document.getElementById('image');
+
+const switch = document={.getElementById('button');}
+
+
+// gestiamo il cambio immagine
+Image.addEventListener('click', function(){
+    
+}
