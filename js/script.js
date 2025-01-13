33329679e5ecc43e335c4f78fae80eeb5b5de44b
ValentinoBellini <valentinobellini.dev@gmail.com>
2025-01-13 09:44:29 +0100

    feat: js arrow function + arrow function and toogle

--- js/script.js
+++ js/script.js
@@ -4,19 +4,53 @@ const lamp = document.getElementById('image');
 const button = document.getElementById('button');
 
 
-// gestiamo il cambio immagine
-button.addEventListener('click', function(){
-    const isOff = lamp.src.includes('white_lamp') && button.classList.contains('off');
+// gestiamo il cambio immagine con sintassi funzione estesa
+
+// button.addEventListener('click', function(){
+//     const isOff = lamp.src.includes('white_lamp') && button.classList.contains('off');
     
-    if(isOff){
+//     if(isOff){
+//         lamp.src = './img/yellow_lamp.png';
+//         button.className = 'on';
+//         button.innerHTML = 'SPEGNI';
+//     } else {
+//         lamp.src = './img/white_lamp.png';
+//         button.className = 'off';
+//         button.innerHTML = 'ACCENDI';
+//     }
+// });
+
+
+
+// con arrow function
+
+// button.addEventListener('click', () =>{
+//     const isOff = lamp.src.includes('white_lamp') && button.classList.contains('off');
+    
+//     if(isOff){
+//         lamp.src = './img/yellow_lamp.png';
+//         button.className = 'on';
+//         button.innerHTML = 'SPEGNI';
+//     } else {
+//         lamp.src = './img/white_lamp.png';
+//         button.className = 'off';
+//         button.innerHTML = 'ACCENDI';
+//     }
+// });
+
+
+
+//con arrow function e metodo toogle
+
+button.addEventListener('click', () => {
+    
+    button.classList.toggle('on');
+
+    if (button.classList.contains('on')) {
         lamp.src = './img/yellow_lamp.png';
-        button.className = 'on';
         button.innerHTML = 'SPEGNI';
     } else {
         lamp.src = './img/white_lamp.png';
-        button.className = 'off';
         button.innerHTML = 'ACCENDI';
     }
 });
-
-
